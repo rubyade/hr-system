260cb34c5d9f69335d0527e932b7299297f8ad5d
mwebaze-nicho <nickmwebaze@gmail.com>
2024-05-27 15:11:08 +0300

    Fixed conflicts

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -1,22 +1,23 @@
-import '@fortawesome/fontawesome-svg-core/styles.css';
-import { config } from '@fortawesome/fontawesome-svg-core';
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
 config.autoAddCss = false;
-import './globals.css';
-import { AppProvider } from '../components/context';
-import Navbar from '@/components/Navbar';
-import { SwrProviders } from './providers';
-import * as React from 'react';
-import { Providers } from './providers';
+import "./globals.css";
+
+import Navbar from "@/components/Navbar";
+import { SwrProviders } from "./providers";
+import * as React from "react";
+import { Providers } from "./providers";
+import { AppProvider } from "@/components/general/context";
 
 export const metadata = {
-  title: 'HR-SYSTEM',
-  description: 'Simplifying HR services',
+  title: "HR-SYSTEM",
+  description: "Simplifying HR services",
 };
+
 
 export default function RootLayout({ children }) {
   return (
-    <html lang='en'>
-      {/* className='bg-gradient-to-r from-red-300 to-purple-500 */}
+    <html lang="en" className="bg-gradient-to-r from-red-300 to-purple-500 ">
       <body>
         <Providers>
           <AppProvider>
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -42,114 +42,92 @@ function Navbar() {
 
   return (
     <>
-<<<<<<< HEAD
-      {
-        <div className="w-full fixed top-0 left-0 shadow-lg mb-20">
-=======
-      {userRole !== "admin" && (
-        <div className="w-full fixed top-0 left-0 shadow-lg">
->>>>>>> 5e06aa1978451bd09aed12983596afe02932262e
-          <nav className="md:px-10 py-4 px-7 md:flex justify-between items-center font-teachers  bg-gradient-to-r from-red-300 to-purple-500">
-            {/* Logo Here!! */}
-            <div className='flex text-2xl cursor-pointer items-center gap-2'>
+      <div className="w-full fixed top-0 left-0 shadow-lg mb-20">
+        <nav className="md:px-10 py-4 px-7 md:flex justify-between items-center font-teachers bg-gradient-to-r from-red-300 to-purple-500">
+          {/* Logo Here!! */}
+          <div className="flex text-2xl cursor-pointer items-center gap-2">
+            <FontAwesomeIcon
+              icon={faBusinessTime}
+              size="1.5x"
+              className="fill-current text-white"
+            />
+  
+            <Link href="/">
+              <h2 className="text-left font-bold text-xl font-jaro text-white xl:text-2xl">
+                Hr-GroupA
+              </h2>
+            </Link>
+          </div>
+  
+          {/* Menu Icon */}
+          <div
+            onClick={handleClick}
+            className="absolute top-4 right-6 cursor-pointer md:hidden"
+          >
+            {clicked ? (
               <FontAwesomeIcon
-                icon={faBusinessTime}
-                size='1.5x'
-                className='fill-current text-white'
+                icon={faXmark}
+                size="2x"
+                className="fill-current text-white"
               />
-
-              <Link href={"/"}>
-                <h2 className="text-left font-bold text-xl font-jaro text-white xl:text-2xl">
-                  Hr-GroupA
-                </h2>
-              </Link>
-
-            </div>
-
-            {/* Menu Icon */}
-            <div
-              onClick={handleClick}
-              className='absolute top-4 right-6 cursor-pointer md:hidden'
-            >
-              {clicked ? (
-                <FontAwesomeIcon
-                  icon={faXmark}
-                  size='2x'
-                  className='fill-current text-white'
-                />
-              ) : (
-                <FontAwesomeIcon
-                  icon={faBars}
-                  size='2x'
-                  className='fill-current text-white'
-                />
-              )}
-            </div>
-
-            {/* Nav-Links here!! */}
-            <ul
-              id='navbar-link'
-              className={`md:flex md:pl-0 pl-9 my-2 md:flex-row md:items-center md:pb-0 absolute md:static bg-gradient-to-r from-red-300 to-purple-500 md:bg-none md:z-auto z-[-1] left-0 w-full md:w-auto transition-all duration-500 ease-in ${
-                clicked ? 'top-12' : 'top-[-490px]'
-              }`}
-            >
-              {!isAuthenticated && (
-                <>
-                  {' '}
-                  <li className='navLink'>
-                    <Link href={'/'}>Home</Link>
-                  </li>
-                  <li className='navLink'>
-                    <Link href={'/about'}>About</Link>
-                  </li>
-                  <li className='navLink'>
-                    <Link href={'/contact'}>Contact</Link>
-                  </li>
-                  <Link
-                    href={'/login'}
-                    className='py-1 md:static md:ml-8'
-                    passHref
-                  >
-                    <Button label='Get Started' />
-                  </Link>
-                </>
-              )}
-
-              {isAuthenticated && userRole !== "admin" && (
-                <>
-<<<<<<< HEAD
-=======
-
-                  {/* <li className="navLink">
-                    <Link href={"/user/work"}>Work</Link>
-                  </li> */}
->>>>>>> 5e06aa1978451bd09aed12983596afe02932262e
-                  <li href={""} className="navLink">
-                    <CheckinButton
-                      label={checkinId ? "Checkout" : "Checkin"}
-                      action={checkinId ? "checkout" : "checkin"}
-                    />
-
-                  </li>
-                  <li className="navLink">
-                    <Link href={"/leave"}>Leave</Link>
-                  </li>
-
-                  <li className="navLink">
-                    <Link href={"/user/update"}>Update</Link>
-
-                  </li>
-                  <li href={''} className='py-1 md:static md:ml-8' passHref>
-                    <Button label='Logout' action='logout' />
-                  </li>
-                </>
-              )}
-            </ul>
-          </nav>
-        </div>
-      }
+            ) : (
+              <FontAwesomeIcon
+                icon={faBars}
+                size="2x"
+                className="fill-current text-white"
+              />
+            )}
+          </div>
+  
+          {/* Nav-Links here!! */}
+          <ul
+            id="navbar-link"
+            className={`md:flex md:pl-0 pl-9 my-2 md:flex-row md:items-center md:pb-0 absolute md:static bg-gradient-to-r from-red-300 to-purple-500 md:bg-none md:z-auto z-[-1] left-0 w-full md:w-auto transition-all duration-500 ease-in ${
+              clicked ? 'top-12' : 'top-[-490px]'
+            }`}
+          >
+            {!isAuthenticated && (
+              <>
+                <li className="navLink">
+                  <Link href="/">Home</Link>
+                </li>
+                <li className="navLink">
+                  <Link href="/about">About</Link>
+                </li>
+                <li className="navLink">
+                  <Link href="/contact">Contact</Link>
+                </li>
+                <Link href="/login" className="py-1 md:static md:ml-8" passHref>
+                  <Button label="Get Started" />
+                </Link>
+              </>
+            )}
+  
+            {isAuthenticated && userRole !== "admin" && (
+              <>
+                <li className="navLink">
+                  <CheckinButton
+                    label={checkinId ? "Checkout" : "Checkin"}
+                    action={checkinId ? "checkout" : "checkin"}
+                  />
+                </li>
+                <li className="navLink">
+                  <Link href="/leave">Leave</Link>
+                </li>
+                <li className="navLink">
+                  <Link href="/user/update">Update</Link>
+                </li>
+                <li className="py-1 md:static md:ml-8" passHref>
+                  <Button label="Logout" action="logout" />
+                </li>
+              </>
+            )}
+          </ul>
+        </nav>
+      </div>
     </>
   );
+  
 }
 
 export default Navbar;
